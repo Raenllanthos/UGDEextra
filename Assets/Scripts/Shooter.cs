64cca764eb637dd6ed60460a593547b8ee8081ae
Raenllanthos <13183449+Raenllanthos@users.noreply.github.com>
2020-09-30 00:03:50 -0400

    Initial commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public float power = 1500f;
+    public float moveSpeed = 5f;
+    Vector2 rotation = new Vector2 (0,0);
+    public float speed = 3;
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += Input.GetAxis("Mouse Y");
+        //float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        //float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        //transform.Translate (h, 0, v);
+        transform.eulerAngles = (Vector2)rotation * speed;
+        if (Input.GetButtonUp ("Fire1")) 
+        {
+            Rigidbody instance = Instantiate (bullet, transform.position, transform.rotation) as Rigidbody;
+            Vector3 forward = transform.TransformDirection (Vector3.forward);
+            instance.AddForce(forward * power);
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# UGDEextra
+ From Chapter 2 in a book, and it was only covered in a video since everything else was outdated.
